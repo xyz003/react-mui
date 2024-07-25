a024758209c3f5667324e8c8559c758e05667769
xyz003 <105212989+xyz003@users.noreply.github.com>
2024-07-25 14:20:20 +0900

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,6 @@
 const Home = () => {
     return (
-        <div>
-            <b>gmail: it.dev0116@gmail.com</b>
-            <b>skybe: live:.cid.8f5e31c5032d28aa</b>
-            <b>telegram: httbs://t.me/betcoder</b>
-            <br />
-            <p>
-                Hello, How are you?
-                Please contact me.
-                Actually, Upwork communication is not good and it's not safe if an account is closed.
-                I am looking forward to your reply.
-
-                And please don't mention about this on upwork.
-                Please just say: "I checked, but not good."
-
-                Thank you.
-            </p>
-        </div>
+        <div>Home</div>
     )
 };
 
